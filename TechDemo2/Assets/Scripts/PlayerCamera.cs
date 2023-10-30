4f6c5365c668e149696eafc8f687fe5f82085d67
Nathan <nat0tpsn@gmail.com>
2023-10-30 22:50:41 +0000

    Upload

---
+++ TechDemo2/Assets/Scripts/PlayerCamera.cs
@@ -0,0 +1,34 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerCamera : MonoBehaviour
+{
+    public float SensX;
+    public float SensY;
+
+    public Transform Orientation;
+
+    private float XRot;
+    private float YRot;
+
+
+    private void Start()
+    {
+        Cursor.lockState = CursorLockMode.Locked;
+        Cursor.visible = false;
+    }
+
+    private void Update()
+    {
+        float mouseX = Input.GetAxisRaw("Mouse X") * Time.deltaTime * SensX;
+        float mouseY = Input.GetAxisRaw("Mouse Y") * Time.deltaTime * SensY;
+
+        YRot += mouseX;
+        XRot -= mouseY;
+        XRot = Mathf.Clamp(XRot, -90f, 90f);
+
+        transform.rotation = Quaternion.Euler(XRot, YRot, 0);
+        Orientation.rotation = Quaternion.Euler(0, YRot, 0);
+    }
+}
---
+++ TechDemo2/Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,115 @@
+using System.Collections;
+using System.Collections.Generic;
+using TMPro;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    [Header("Movement")]
+    public float moveSpeed;
+
+    public float groundDrag;
+
+    public float jumpForce;
+    public float jumpCooldown;
+    public float airMultiplier;
+    bool CanJump;
+
+    [HideInInspector] public float walkSpeed;
+    [HideInInspector] public float sprintSpeed;
+
+    [Header("Player Controls")]
+    public KeyCode Jump = KeyCode.Space;
+
+    [Header ("Grounded")]
+    public float playerHeight;
+    public LayerMask Ground;
+    bool grounded;
+
+
+    public Transform orientation;
+
+    private float horizontalInput;
+    private float verticalInput;
+
+    Vector3 MovementDir;
+
+    Rigidbody rb;
+
+    private void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+        rb.freezeRotation = true;
+
+        CanJump = true;
+    }
+
+    private void Update()
+    {
+        // ground check
+        //grounded = Physics.Raycast(transform.position + new Vector3(0, 0.05f, 0), Vector3.down, playerHeight * 0.5f + 0.3f, Ground);
+        grounded = Physics.Raycast(transform.position, Vector3.down, playerHeight * 0.5f + 0.3f, Ground);
+
+        PlayerInput();
+        MoveSpeed();        
+        if (grounded)
+            rb.drag = groundDrag;
+        else
+            rb.drag = 0;
+    }
+
+    private void FixedUpdate()
+    {
+        MovePlayer();
+    }
+
+    private void PlayerInput()
+    {
+        horizontalInput = Input.GetAxisRaw("Horizontal");
+        verticalInput = Input.GetAxisRaw("Vertical");
+
+        // when to jump
+        if (Input.GetKey(Jump) && CanJump && grounded)
+        {
+            CanJump = false;
+
+            JumpKey();
+
+            Invoke(nameof(ResetJump), jumpCooldown);
+        }
+    }
+
+    private void MovePlayer()
+    {
+        MovementDir = orientation.forward * verticalInput + orientation.right * horizontalInput;
+               
+        if (grounded)
+            rb.AddForce(MovementDir.normalized * moveSpeed * 10f, ForceMode.Force);
+          
+        else if (!grounded)
+            rb.AddForce(MovementDir.normalized * moveSpeed * 10f * airMultiplier, ForceMode.Force);
+    }
+
+    private void MoveSpeed()
+    {
+        Vector3 flatVel = new Vector3(rb.velocity.x, 0f, rb.velocity.z);
+
+        if (flatVel.magnitude > moveSpeed)
+        {
+            Vector3 limitedVel = flatVel.normalized * moveSpeed;
+            rb.velocity = new Vector3(limitedVel.x, rb.velocity.y, limitedVel.z);
+        }                
+    }
+
+    private void JumpKey()
+    {
+        
+        rb.velocity = new Vector3(rb.velocity.x, 0f, rb.velocity.z);
+
+        rb.AddForce(transform.up * jumpForce, ForceMode.Impulse);
+    }
+    private void ResetJump()
+    {
+        CanJump = true;
+    }
+}
