6c2ea788ed66129df32721125c4294312462751f
Nathan <nat0tpsn@gmail.com>
2023-11-01 16:17:27 +0000

    Changes

--- TechDemo2/Assets/Scripts/ItemPickUp.cs
+++ TechDemo2/Assets/Scripts/ItemPickUp.cs
@@ -1,21 +1,29 @@
 using System.Collections;
 using System.Collections.Generic;
-using Unity.VisualScripting;
 using UnityEngine;
 
-public class ItemPickUp : MonoBehaviour
+public class ItemPickup : Interactable
 {
-    public Item Item;
 
+    public Item item;   // Item to put in the inventory on pickup
 
+    // When the player interacts with the item
+    public override void Interact()
+    {
+        base.Interact();
+
+        PickUp();   // Pick it up!
+    }
+
+    // Pick up the item
     void PickUp()
     {
-        InventoryManager.Instance.Add(Item);
-        Destroy(gameObject);
+        Debug.Log("Picking up " + item.name);
+        bool wasPickedUp = Inventory.instance.Add(item);    // Add to inventory
+
+        // If successfully picked up
+        if (wasPickedUp)
+            Destroy(gameObject);    // Destroy item from scene
     }
 
-    private void OnMouseDown()
-    {
-        PickUp();
-    }
 }
