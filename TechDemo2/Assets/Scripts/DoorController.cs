bd226acb1b5fc51771d4f0a03a5a7dfbc3ed9dee
Nathan <nat0tpsn@gmail.com>
2023-11-03 01:43:11 +0000

    Keypad

---
+++ TechDemo2/Assets/Scripts/DoorController.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DoorController : MonoBehaviour
+{
+    public bool isLockedByPassword;
+
+    public Animator anim;
+
+    public void OpenClose()
+    {
+        if (isLockedByPassword)
+        {
+            Debug.Log("Locked by password");
+            return;
+        }
+
+        anim.SetTrigger("DoorOpen");
+    }
+
+}
